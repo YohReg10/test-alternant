42a3797e4f188ef05d6d5e54b1ac5656907aadb0
Yohan <yohan-fmr@hotmail.com>
2024-12-14 15:05:10 +0100

    dependance + gitignore update

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+// pages/index.tsx
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Bienvenue sur mon site !</h1>
+    </div>
+  );
+};
+
+export default HomePage;

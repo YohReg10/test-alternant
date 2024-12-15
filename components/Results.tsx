a57b7394f9376d808e84f74deb4cac390b73ae24
Yohan <yohan-fmr@hotmail.com>
2024-12-15 14:06:34 +0100

    create component Results

---
+++ components/Results.tsx
@@ -0,0 +1,15 @@
+function Results() {
+    return (
+        <div
+            id="results-section"
+            className="w-1/2 bg-gray-100 shadow-md p-4 rounded-lg ml-4"
+        >
+            <h2 className="text-xl font-bold mb-4">Résultats</h2>
+            <p className="text-gray-700">
+                Les résultats de votre recherche apparaîtront ici.
+            </p>
+        </div>
+    );
+}
+
+export default Results;

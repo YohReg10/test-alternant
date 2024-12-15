81cd0723230f7e83a06ce2d0ae17f435be94c3fc
Yohan <yohan-fmr@hotmail.com>
2024-12-15 18:56:07 +0100

    deleted puces and add Distances name

--- components/Checklist.tsx
+++ components/Checklist.tsx
@@ -2,21 +2,21 @@ function Checklist() {
     return (
         <div
             id="checklist-section"
-            className="w-full bg-white shadow-md p-0 rounded-lg mr-0"
+            className="w-full bg-white shadow-md p-4 rounded-lg"
         >
-            <h2 className="text-xl font-bold mb-4">Checklist</h2>
-            <ul className="list-disc pl-5">
-                <li>
-                    <input type="checkbox" id="item1" className="mr-2" />
-                    <label htmlFor="item1">Activité 1</label>
+            <h2 className="text-xl font-bold mb-4">Distances</h2>
+            <ul className="list-none pl-0">
+                <li className="flex items-center space-x-2 mb-2">
+                    <input type="checkbox" id="item1" className="w-5 h-5" />
+                    <label htmlFor="item1" className="leading-5">&lt;5km</label>
                 </li>
-                <li>
-                    <input type="checkbox" id="item2" className="mr-2" />
-                    <label htmlFor="item2">Activité 2</label>
+                <li className="flex items-center space-x-2 mb-2">
+                    <input type="checkbox" id="item2" className="w-5 h-5" />
+                    <label htmlFor="item2" className="leading-5">&lt;15km</label>
                 </li>
-                <li>
-                    <input type="checkbox" id="item3" className="mr-2" />
-                    <label htmlFor="item3">Activité 3</label>
+                <li className="flex items-center space-x-2">
+                    <input type="checkbox" id="item3" className="w-5 h-5" />
+                    <label htmlFor="item3" className="leading-5">&lt;25km</label>
                 </li>
             </ul>
         </div>

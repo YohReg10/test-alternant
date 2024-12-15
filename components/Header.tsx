488c98b98a0f6b9d1f7d43568386dfc9079575aa
Yohan <yohan-fmr@hotmail.com>
2024-12-15 13:35:00 +0100

    organize Header and add parent div + add id in index

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,31 +1,37 @@
-
 function Header() {
     return (
-        <header className="flex justify-between items-center bg-gray-200 w-full">
-        {/* Conteneur principal avec flex pour aligner les éléments */}
-        <div className="w-1/3">
-            <img
-            src="/images/beach.jpg"
-            alt="Musée"
-            className="w-full h-64 object-cover"
-            />
-        </div>
-        <div className="w-1/3">
-            <img
-            src="/images/montpellier.jpg"
-            alt="Musée"
-            className="w-full h-64 object-cover"
-            />
-        </div>
-        <div className="w-1/3">
-            <img
-            src="/images/family.jpg"
-            alt="Musée"
-            className="w-full h-64 object-cover"
-            />
-        </div>
+        <header className="bg-gray-200 w-full">
+            {/* Conteneur parent pour toutes les images */}
+            <div className="flex w-full">
+                {/* Première image */}
+                <div className="w-1/3">
+                    <img
+                        src="/images/beach.jpg"
+                        alt="Plage"
+                        className="w-full h-64 object-cover"
+                    />
+                </div>
+
+                {/* Deuxième image */}
+                <div className="w-1/3">
+                    <img
+                        src="/images/montpellier.jpg"
+                        alt="Montpellier"
+                        className="w-full h-64 object-cover"
+                    />
+                </div>
+
+                {/* Troisième image */}
+                <div className="w-1/3">
+                    <img
+                        src="/images/family.jpg"
+                        alt="Famille"
+                        className="w-full h-64 object-cover"
+                    />
+                </div>
+            </div>
         </header>
     );
 }
+
 export default Header;
-  
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,16 @@ import Header from "../components/Header";
 
 export default function HomePage() {
   return (
-    <div className='bg-blue-50 text-black'>
-      <Header />
-      <h1 className="text-center text-3xl p-3">Activités et Aventures à Montpellier</h1>
+    <div id="layout-page">
+      <div id = "header-ruban" className='bg-blue-50 text-black'>
+        <Header />
+        <h1 className="text-center text-3xl p-3"><strong>Activités et Aventures à Montpellier</strong></h1>
+      </div>
+      <div id='site-content'>
+        
+
+      </div>
     </div>
+    
   );
 }

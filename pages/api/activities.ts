35affc110f7a2d9fb553dd087f7c6bb990df60b6
Yohan <yohan-fmr@hotmail.com>
2025-01-30 13:52:15 +0100

    try another test

--- pages/api/activities.ts
+++ pages/api/activities.ts
@@ -22,8 +22,14 @@ export default async function handler(req, res) {
         }
       });
 
-      console.log("Activities fetched successfully", activities);
-      res.status(200).json(activities);
+      // Ajouter un contrôle pour vérifier que activities est bien un tableau
+      if (Array.isArray(activities)) {
+        console.log("Activities fetched successfully", activities);
+        res.status(200).json(activities);
+      } else {
+        console.error("Activities is not an array", activities);
+        res.status(500).json({ error: 'Unexpected response format' });
+      }
     } catch (error) {
       console.error("Error fetching activities:", error);
       res.status(500).json({ error: 'Failed to fetch activities' });
